bfba841b81cc336666482c3a8e6899ac81447342
kostya007 <kostya.1998.nosarev@mail.ru>
2019-07-14 18:19:18 +0700

    Add new todo

--- src/components/item-add-form/item-add-form.js
+++ src/components/item-add-form/item-add-form.js
@@ -4,13 +4,32 @@ import './item-add-form.css';
 
 export default class ItemAddForm extends Component {
 
+    state = {
+        label: ''
+    };
+
+    onLabelChange = (e) => {
+        this.setState({
+            label: e.target.value
+        });
+    };
+
+    onSubmit = (e) => {
+        e.preventDefault();
+        this.props.onItemAdded(this.state.label);
+    };
+
     render () {
         return (
-            <div className="item-add-form">
+            <form className="item-add-form d-flex"
+                onSubmit={this.onSubmit}>
+                <input type="text" 
+                    className="form-control"
+                    onChange={ this.onLabelChange }
+                    placeholder="What needs to input done" />
                 <button 
-                    className="btn btn-outline-secondary"
-                    onClick={ () => { this.props.onItemAdded('Hello world') } }>Add Item</button>
-            </div>
+                    className="btn btn-outline-secondary">Add Item</button>
+            </form>
         );
     }
 }
